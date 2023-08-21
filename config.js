0fc31080dd1a14529ca6232f4939d1547e81ce5b
Jesse Storbeck <jesse.storbeck@bc.edu>
2023-08-21 12:15:01 -0400

    initial commit

---
+++ config.js
@@ -0,0 +1,11 @@
+// Set configurations for experiment
+
+const experimentConfig = {
+    fontColor: "black",
+    fontSize: "20px",
+    fontFamily: "'Open Sans', 'Arial', sans-serif",
+    comprehension: true,
+    correctiveFeedback: true // Only relevant if comprehension is set to true
+}
+
+export default experimentConfig;
